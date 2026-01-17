37f6712e968219b42d6f9535f8c07a3170bb53aa
pawanxandromeda <pawanxandromeda@gmail.com>
2026-01-17 20:48:55 +0530

    url update

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.use(
     origin: (origin, callback) => {
       const allowedOrigins = [
         "http://localhost:5173",
-        "https://alpex-git-main-pawanxandromedas-projects.vercel.app",
+        "https://alpex-f5hk.vercel.app",
         "https://alpex-customers.onrender.com",
       ];
       if (!origin || allowedOrigins.includes(origin)) {
