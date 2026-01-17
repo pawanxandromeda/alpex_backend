5e080afaad833b129598a54de7408b474649543c
pawanxandromeda <pawanxandromeda@gmail.com>
2026-01-17 18:48:06 +0530

    commit

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.use(
     origin: (origin, callback) => {
       const allowedOrigins = [
         "http://localhost:5173",
-        "https://alpex-software.onrender.com",
+        "https://alpex-git-main-pawanxandromedas-projects.vercel.app",
         "https://alpex-customers.onrender.com",
       ];
       if (!origin || allowedOrigins.includes(origin)) {
